763cd5e88e33f8493c318f97f44e7bcaa4a30a97
Shea Thomas <email": null>
2017-06-16 12:44:00 -0400

    Enable Pacman to move

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,47 @@ $(document).ready(function() {
 
   $(document).on("keydown", handleTyping);
 
+  var pacman = {};
+
+  pacman.r = 20;
+  pacman.d = 5;
+  pacman.l = 20
+  pacman.u = 5
+
+
   function handleTyping(event) {
-    console.log(event.which);
-  };
+
+    pacman.r += 10
+    pacman.d += 10
+    pacman.l -= 10
+    pacman.u -= 10
+
+    switch(event.which) {
+      case 39:
+        $("#pacman").css({
+          "left": pacman.r + "px"
+        });
+        break;
+      case 40:
+      $("#pacman").css({
+        "top": pacman.d + "px"
+      });
+        break;
+      case 37:
+      $("#pacman").css({
+        "left": pacman.l + "px"
+      });
+        break;
+      case 38:
+      $("#pacman").css({
+        "top": pacman.u + "px"
+      });
+        break;
+      };
+
+    };
+
+
 });
+
+// trbl = 38 39 40 37

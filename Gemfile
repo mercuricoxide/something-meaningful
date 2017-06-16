dfa6d54e999fdcf54cefc7b3a7800978e95e3ca4
Shea Thomas <email": null>
2017-06-16 11:03:20 -0400

    Build pacman game baord and character model

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'rake'
 
 gem 'shotgun', '~>0.9.1'
 gem 'bcrypt'
+gem 'pry'
 
 group :test do
   gem 'shoulda-matchers'
--- app/controllers/message.rb
+++
@@ -1,9 +0,0 @@
-get "/messages" do
-  @messages = Message.all
-  erb :"index"
-end
-
-get "/messages/new" do
-  redirect "/" unless session[:user_id]
-  erb :"messages/_message_form"
-end
